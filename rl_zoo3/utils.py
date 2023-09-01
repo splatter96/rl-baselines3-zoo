bc324f9537b28e22448aac4d8c9310ac9e1a28f0
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2023-09-01 15:12:03 +0200

    Added sacd to usable algorithms and addded hyperparameter file for mspacman and sacd

--- rl_zoo3/utils.py
+++ rl_zoo3/utils.py
@@ -13,7 +13,7 @@ import yaml
 from gymnasium import spaces
 from huggingface_hub import HfApi
 from huggingface_sb3 import EnvironmentName, ModelName
-from sb3_contrib import ARS, QRDQN, TQC, TRPO, RecurrentPPO
+from sb3_contrib import ARS, QRDQN, TQC, TRPO, RecurrentPPO, SACD
 from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.callbacks import BaseCallback
@@ -37,6 +37,7 @@ ALGOS: Dict[str, Type[BaseAlgorithm]] = {
     "tqc": TQC,
     "trpo": TRPO,
     "ppo_lstm": RecurrentPPO,
+    "sacd": SACD,
 }
 
 
